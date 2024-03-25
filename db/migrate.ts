0180acce9bf20d9a2f70647aa28532a223aefd7e
Mislav <mislavjc@gmail.com>
2024-03-25 09:37:39 +0100

    feat: add db commands to package.json

--- db/migrate.ts
+++ db/migrate.ts
@@ -1,11 +1,15 @@
+import { neon } from '@neondatabase/serverless';
 import dotenv from 'dotenv';
+import { drizzle } from 'drizzle-orm/neon-http';
 import { migrate } from 'drizzle-orm/neon-http/migrator';
-
-import { db } from './drizzle';
 
 dotenv.config({
   path: '.env.local',
 });
+
+const sql = neon(process.env.DATABASE_URL!);
+
+const db = drizzle(sql);
 
 const runMigration = async () => {
   await migrate(db, {
