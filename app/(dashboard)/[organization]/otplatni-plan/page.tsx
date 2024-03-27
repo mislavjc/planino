4a841234fea6d48440a3e888e1278130c46a53bb
Mislav <mislavjc@gmail.com>
2024-03-27 21:02:26 +0100

    feat: add scroll area

--- app/(dashboard)/[organization]/otplatni-plan/page.tsx
+++ app/(dashboard)/[organization]/otplatni-plan/page.tsx
@@ -2,6 +2,7 @@ import { getLoans } from 'actions/loan';
 import { Metadata } from 'next';
 
 import { Card, CardContent, CardHeader } from 'ui/card';
+import { ScrollArea, ScrollBar } from 'ui/scroll-area';
 import { TypographyH3, TypographyP } from 'ui/typography';
 
 import { AddRow } from 'components/@loans/add-row';
@@ -22,26 +23,31 @@ const PaymentPlanPage = async ({
 
   return (
     <div>
-      <Card className="md:w-1/2">
+      <Card className="w-full md:w-1/2">
         <CardHeader>
           <TypographyH3>Otplatni plan</TypographyH3>
         </CardHeader>
-        <CardContent>
-          <div className="grid grid-cols-7 text-sm">
-            <TypographyP className="col-span-2">Naziv</TypographyP>
-            <TypographyP className="text-right">Kamata</TypographyP>
-            <TypographyP className="text-right">Broj rata</TypographyP>
-            <TypographyP className="text-right">Iznos</TypographyP>
-            <TypographyP className="col-span-2 text-right">
-              Prvi mjesec
-            </TypographyP>
-          </div>
-          <div>
-            {loans.map((loan) => (
-              <Row key={loan.loanId} {...loan} />
-            ))}
-          </div>
-          <AddRow organization={organization} />
+        <CardContent className="overflow-x-auto">
+          <ScrollArea className="min-w-[40rem]">
+            <div>
+              <div className="grid grid-cols-7 text-sm">
+                <TypographyP className="col-span-2">Naziv</TypographyP>
+                <TypographyP className="text-right">Kamata</TypographyP>
+                <TypographyP className="text-right">Broj rata</TypographyP>
+                <TypographyP className="text-right">Iznos</TypographyP>
+                <TypographyP className="col-span-2 text-right">
+                  Prvi mjesec
+                </TypographyP>
+              </div>
+              <div>
+                {loans.map((loan) => (
+                  <Row key={loan.loanId} {...loan} />
+                ))}
+              </div>
+              <AddRow organization={organization} />
+            </div>
+            <ScrollBar />
+          </ScrollArea>
         </CardContent>
       </Card>
     </div>
