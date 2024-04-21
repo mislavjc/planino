32bb315f349fb1510b7d4f26c5f4768742265c0d
Mislav <mislavjc@gmail.com>
2024-04-21 17:51:12 +0200

    feat: improve add row button

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -5,6 +5,7 @@ import { Loader2, Plus } from 'lucide-react';
 
 import { FocusBorder } from './table/focus-border';
 import { Button } from './ui/button';
+import { TypographyP } from './ui/typography';
 
 export const SubmitButton = () => {
   const status = useFormStatus();
@@ -12,15 +13,18 @@ export const SubmitButton = () => {
   return (
     <Button
       type="submit"
-      className="group relative w-full border"
-      variant="secondary"
+      className="group relative flex w-full justify-start"
+      variant="outline"
       disabled={status.pending}
       ring={false}
     >
       {status.pending ? (
         <Loader2 className="mr-2 size-4 animate-spin" />
       ) : (
-        <Plus size={16} />
+        <div className="flex items-center gap-2">
+          <Plus size={16} />
+          <TypographyP>Dodaj redak</TypographyP>
+        </div>
       )}
       <FocusBorder />
     </Button>
