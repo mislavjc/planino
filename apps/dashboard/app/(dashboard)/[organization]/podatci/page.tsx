3d4166b7501e3e37a86592a27bfc3b5a2b889b98
Mislav <mislavjc@gmail.com>
2024-04-27 19:19:31 +0200

    feat: find table bounds and color inside of them

--- apps/dashboard/app/(dashboard)/[organization]/podatci/page.tsx
+++ apps/dashboard/app/(dashboard)/[organization]/podatci/page.tsx
@@ -4,11 +4,67 @@ import { ScrollArea } from 'components/ui/scroll-area';
 
 import { importer } from 'api/importer/client';
 
+interface TableCoordinates {
+  startRow: number;
+  startColumn: number;
+  endRow: number;
+  endColumn: number;
+}
+
+const generateIndexArray = (
+  coordinates: TableCoordinates,
+): [number, number][] => {
+  const { startRow, endRow, startColumn, endColumn } = coordinates;
+  let indexArray: [number, number][] = [];
+
+  for (let row = startRow; row <= endRow; row++) {
+    for (let col = startColumn; col <= endColumn; col++) {
+      indexArray.push([row, col]);
+    }
+  }
+
+  return indexArray;
+};
+
+const isCellInAnyTable = (
+  cellRow: number,
+  cellColumn: number,
+  tables: TableCoordinates[],
+): [boolean, number] => {
+  for (let i = 0; i < tables.length; i++) {
+    const indexes = generateIndexArray(tables[i]);
+    if (indexes.some(([row, col]) => row === cellRow && col === cellColumn)) {
+      return [true, i];
+    }
+  }
+  return [false, -1];
+};
+const predefinedColors = [
+  'hsla(0, 100%, 70%, 0.15)', // Red Pink
+  'hsla(30, 100%, 75%, 0.15)', // Peach
+  'hsla(60, 100%, 85%, 0.15)', // Pale Yellow
+  'hsla(90, 100%, 80%, 0.15)', // Tea Green
+  'hsla(180, 100%, 75%, 0.15)', // Baby Blue
+  'hsla(210, 100%, 80%, 0.15)', // Blue Mauve
+  'hsla(270, 100%, 80%, 0.15)', // Mauve
+  'hsla(300, 100%, 80%, 0.15)', // Pink
+  'hsla(60, 100%, 95%, 0.15)', // Ivory
+  'hsla(150, 60%, 75%, 0.15)', // Eton Blue
+];
+
+const getColorByTableIndex = (index: number) => {
+  if (index === -1) {
+    return 'transparent';
+  }
+
+  return predefinedColors[index % predefinedColors.length];
+};
+
 const DataPage = async () => {
   const { data, error } = await importer.GET('/import/{file}/coordinates', {
     params: {
       path: {
-        file: 'five.xlsx',
+        file: 'medium.xlsx',
       },
     },
   });
@@ -20,7 +76,7 @@ const DataPage = async () => {
   const maxCols = Math.max(...data.worksheet.map((row) => row.length));
 
   return (
-    <div className="max-h-[60vh] max-w-screen-xl overflow-auto">
+    <div className="max-h-[80vh] max-w-screen-xl overflow-auto">
       <ScrollArea className="w-max">
         <div
           className="grid"
@@ -30,22 +86,44 @@ const DataPage = async () => {
         >
           {data.worksheet.map((row, rowIndex) => (
             <React.Fragment key={rowIndex}>
-              {row.map((cell, cellIndex) => (
-                <div
-                  key={cellIndex}
-                  className="h-10 truncate border border-gray-300 p-2 font-mono"
-                >
-                  {String(cell ?? '')}
-                </div>
-              ))}
+              {row.map((cell, cellIndex) => {
+                const [_isInTable, tableIndex] = isCellInAnyTable(
+                  rowIndex,
+                  cellIndex,
+                  data.tables.map((table) => table.coordinates),
+                );
+
+                return (
+                  <div
+                    key={cellIndex}
+                    className="h-10 truncate border border-gray-300 p-2 font-mono"
+                    style={{
+                      backgroundColor: getColorByTableIndex(tableIndex),
+                    }}
+                  >
+                    {String(cell ?? '')}
+                  </div>
+                );
+              })}
               {row.length < maxCols &&
                 Array.from({ length: maxCols - row.length }).map(
-                  (_, emptyIndex) => (
-                    <div
-                      key={emptyIndex}
-                      className="h-10 border border-gray-300 p-2"
-                    />
-                  ),
+                  (_, emptyIndex) => {
+                    const [_isInTable, tableIndex] = isCellInAnyTable(
+                      rowIndex,
+                      row.length + emptyIndex,
+                      data.tables.map((table) => table.coordinates),
+                    );
+
+                    return (
+                      <div
+                        key={emptyIndex}
+                        className="h-10 truncate border border-gray-300 p-2 font-mono"
+                        style={{
+                          backgroundColor: getColorByTableIndex(tableIndex),
+                        }}
+                      />
+                    );
+                  },
                 )}
             </React.Fragment>
           ))}
