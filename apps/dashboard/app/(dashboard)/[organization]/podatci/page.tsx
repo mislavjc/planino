3b62d8a47726ac67e22f747c225fa36ede230071
Mislav <mislavjc@gmail.com>
2024-04-27 18:33:29 +0200

    feat: display the worksheet

--- apps/dashboard/app/(dashboard)/[organization]/podatci/page.tsx
+++ apps/dashboard/app/(dashboard)/[organization]/podatci/page.tsx
@@ -1,3 +1,7 @@
+import React from 'react';
+
+import { ScrollArea } from 'components/ui/scroll-area';
+
 import { importer } from 'api/importer/client';
 
 const DataPage = async () => {
@@ -13,10 +17,40 @@ const DataPage = async () => {
     throw new Error(error.error);
   }
 
+  const maxCols = Math.max(...data.worksheet.map((row) => row.length));
+
   return (
-    <div>
-      <h1>Organization Data</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div className="max-h-[60vh] max-w-screen-xl overflow-auto">
+      <ScrollArea className="w-max">
+        <div
+          className="grid"
+          style={{
+            gridTemplateColumns: `repeat(${maxCols}, 10rem)`,
+          }}
+        >
+          {data.worksheet.map((row, rowIndex) => (
+            <React.Fragment key={rowIndex}>
+              {row.map((cell, cellIndex) => (
+                <div
+                  key={cellIndex}
+                  className="h-10 truncate border border-gray-300 p-2 font-mono"
+                >
+                  {String(cell ?? '')}
+                </div>
+              ))}
+              {row.length < maxCols &&
+                Array.from({ length: maxCols - row.length }).map(
+                  (_, emptyIndex) => (
+                    <div
+                      key={emptyIndex}
+                      className="h-10 border border-gray-300 p-2"
+                    />
+                  ),
+                )}
+            </React.Fragment>
+          ))}
+        </div>
+      </ScrollArea>
     </div>
   );
 };
