3c018b85613e20197b8ba5bed63672cde3b8bb57
Mislav <mislavjc@gmail.com>
2024-04-28 17:25:02 +0200

    feat: style overview page

--- apps/dashboard/app/(dashboard)/[organization]/podatci/pregled/page.tsx
+++ apps/dashboard/app/(dashboard)/[organization]/podatci/pregled/page.tsx
@@ -1,16 +1,18 @@
 import { Suspense } from 'react';
 import { redirect } from 'next/navigation';
 
+import { Card, CardContent, CardHeader } from 'ui/card';
 import { ScrollArea } from 'ui/scroll-area';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from 'ui/tabs';
+import { TypographyH3 } from 'ui/typography';
 
 import { ExcelTable } from 'components/@import/excel-table';
 
 import { importer } from 'api/importer/client';
 
-export const revalidate = 1;
+export const revalidate = 0;
 
-const DataOverview = async ({
+const DataOverviewPage = async ({
   params: { organization },
 }: {
   params: { organization: string };
@@ -33,30 +35,37 @@ const DataOverview = async ({
 
   return (
     <div>
-      <Tabs defaultValue={data?.objects[0].key}>
-        <TabsList>
-          {data?.objects.map((file) => (
-            <TabsTrigger key={file.key} value={file.key}>
-              {file.key}
-            </TabsTrigger>
-          ))}
-        </TabsList>
-        {data?.objects.map((file) => (
-          <TabsContent
-            className="max-h-[80vh] max-w-screen-xl overflow-auto"
-            key={file.key}
-            value={file.key}
-          >
-            <ScrollArea className="w-max">
-              <Suspense>
-                <ExcelTable file={file.key} />
-              </Suspense>
-            </ScrollArea>
-          </TabsContent>
-        ))}
-      </Tabs>
+      <Card className="max-w-screen-xl">
+        <CardHeader>
+          <TypographyH3>Pregled uvezenih podataka</TypographyH3>
+        </CardHeader>
+        <CardContent>
+          <Tabs defaultValue={data?.objects[0].key}>
+            <TabsList>
+              {data?.objects.map((file) => (
+                <TabsTrigger key={file.key} value={file.key}>
+                  {file.key}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+            {data?.objects.map((file) => (
+              <TabsContent
+                className="max-h-[80vh] max-w-screen-xl overflow-auto"
+                key={file.key}
+                value={file.key}
+              >
+                <ScrollArea className="w-max">
+                  <Suspense>
+                    <ExcelTable file={file.key} />
+                  </Suspense>
+                </ScrollArea>
+              </TabsContent>
+            ))}
+          </Tabs>
+        </CardContent>
+      </Card>
     </div>
   );
 };
 
-export default DataOverview;
+export default DataOverviewPage;
