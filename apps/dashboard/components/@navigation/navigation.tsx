85f4f2df607bfe77bc1286b20cbbbcc3636f2c8f
Mislav <mislavjc@gmail.com>
2024-05-07 16:22:04 +0200

    feat: change nav

--- apps/dashboard/components/@navigation/navigation.tsx
+++ apps/dashboard/components/@navigation/navigation.tsx
@@ -6,23 +6,25 @@ import { Auth } from './auth';
 
 export const Navigation = () => {
   return (
-    <div className="bg-muted/40 sticky top-0 mx-auto max-w-screen-xl px-6 py-4">
-      <div className="flex items-center justify-between">
-        <div>
-          <Link href="/" className="flex items-center gap-2">
-            <Image src="/icon.png" alt="Planino" width={18} height={18} />
-            <span className="text-lg font-semibold">Planino</span>
-          </Link>
+    <div className="sticky top-0 bg-white">
+      <div className="mx-auto max-w-screen-xl px-6 py-4">
+        <div className="flex items-center justify-between">
+          <div>
+            <Link href="/" className="flex items-center gap-2">
+              <Image src="/icon.png" alt="Planino" width={18} height={18} />
+              <span className="text-lg font-semibold">Planino</span>
+            </Link>
+          </div>
+          <Suspense
+            fallback={
+              <div className="flex items-center gap-3">
+                <div className="bg-muted h-10 w-24 animate-pulse rounded-lg" />
+              </div>
+            }
+          >
+            <Auth />
+          </Suspense>
         </div>
-        <Suspense
-          fallback={
-            <div className="flex items-center gap-3">
-              <div className="bg-muted h-10 w-24 animate-pulse rounded-lg" />
-            </div>
-          }
-        >
-          <Auth />
-        </Suspense>
       </div>
     </div>
   );
