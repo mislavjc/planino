e3fe9e2319e4dee15251649796b182be0afa7807
Mislav <mislavjc@gmail.com>
2024-05-07 16:22:20 +0200

    feat: add initial features section

--- apps/dashboard/app/(landing)/page.tsx
+++ apps/dashboard/app/(landing)/page.tsx
@@ -1,9 +1,17 @@
+import { Features } from 'components/@homepage/features';
 import { Hero } from 'components/@homepage/hero';
 
 const HomePage = async () => {
   return (
-    <div className="mx-auto flex max-w-screen-lg flex-col gap-4 px-4">
-      <Hero />
+    <div className="flex flex-col gap-20">
+      <div className="mx-auto flex max-w-screen-lg flex-col gap-4 px-4">
+        <Hero />
+      </div>
+      <div className="bg-muted/40 py-16 px-4">
+        <div className="mx-auto max-w-screen-lg">
+          <Features />
+        </div>
+      </div>
     </div>
   );
 };
