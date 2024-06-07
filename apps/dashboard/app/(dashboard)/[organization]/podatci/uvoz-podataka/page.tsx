599dc4ea9cffb684495bac347746396d552f1a98
Mislav <mislavjc@gmail.com>
2024-06-07 17:22:06 +0200

    feat: show files on first step

--- apps/dashboard/app/(dashboard)/[organization]/podatci/uvoz-podataka/page.tsx
+++ apps/dashboard/app/(dashboard)/[organization]/podatci/uvoz-podataka/page.tsx
@@ -1,18 +1,40 @@
+import { getFiles } from 'actions/importer';
+
 import { Card, CardContent, CardHeader } from 'ui/card';
+import { TypographyH3 } from 'ui/typography';
 
 import { FileDropzone } from 'components/@import/upload-file';
-import { TypographyH3 } from 'components/ui/typography';
 
-const DataImportPage = () => {
+const DataImportPage = async ({
+  params: { organization },
+}: {
+  params: { organization: string };
+}) => {
+  const files = await getFiles(organization);
+
   return (
-    <Card className="max-w-screen-sm">
-      <CardHeader>
-        <TypographyH3>Uvoz podataka</TypographyH3>
-      </CardHeader>
-      <CardContent>
-        <FileDropzone />
-      </CardContent>
-    </Card>
+    <div className="flex flex-col gap-4">
+      <Card className="max-w-screen-sm">
+        <CardHeader>
+          <TypographyH3>Uvoz podataka</TypographyH3>
+        </CardHeader>
+        <CardContent>
+          <FileDropzone />
+        </CardContent>
+      </Card>
+      <Card className="max-w-screen-sm">
+        <CardHeader>
+          <TypographyH3>Pregled uvezenih podataka</TypographyH3>
+        </CardHeader>
+        <CardContent>
+          <ul>
+            {files.map((file) => (
+              <li key={file.name}>{file.name}</li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
