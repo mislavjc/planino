5842a244d4b82accf936785edb9b7d969bd9b3bb
Mislav <mislavjc@gmail.com>
2024-06-10 09:54:19 +0200

    feat: add earnings table

--- apps/dashboard/app/(dashboard)/[organization]/prihodi/page.tsx
+++ apps/dashboard/app/(dashboard)/[organization]/prihodi/page.tsx
@@ -3,7 +3,17 @@ import { BarChart } from '@planino/charts';
 import { getMonthlyEarnings } from 'actions/output';
 
 import { Card, CardContent, CardHeader } from 'ui/card';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from 'ui/table';
 import { TypographyH3 } from 'ui/typography';
+
+import { formatCurrency } from 'lib/utils';
 
 const EarningsPage = async ({
   params: { organization },
@@ -12,8 +22,46 @@ const EarningsPage = async ({
 }) => {
   const earnings = await getMonthlyEarnings(organization);
 
+  const productNames = Object.keys(earnings.values[0]).filter(
+    (key) => key !== 'month',
+  );
+
   return (
     <div className="flex flex-col gap-4">
+      <Card>
+        <CardHeader>
+          <TypographyH3>Prihodi</TypographyH3>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Proizvod</TableHead>
+                {earnings.values.map((value) => (
+                  <TableHead key={value.month} className="text-right">
+                    {value.month}
+                  </TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {productNames.map((productName) => (
+                <TableRow key={productName}>
+                  <TableCell className="font-medium">{productName}</TableCell>
+                  {earnings.values.map((value) => (
+                    <TableCell
+                      key={value.month}
+                      className="text-right font-mono"
+                    >
+                      {formatCurrency(value[productName])}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
       <Card>
         <CardHeader>
           <TypographyH3>Prihodi</TypographyH3>
