4105356087a0968ebd5fcb7939c3bc3c0fba67e9
Mislav <mislavjc@gmail.com>
2024-06-10 02:18:32 +0200

    feat: use cards

--- apps/dashboard/app/(dashboard)/[organization]/racun-dobiti-i-gubitka/page.tsx
+++ apps/dashboard/app/(dashboard)/[organization]/racun-dobiti-i-gubitka/page.tsx
@@ -1,5 +1,6 @@
 import { getMonthlyAggregateCostsAndSales } from 'actions/output';
 
+import { Card, CardContent, CardHeader } from 'ui/card';
 import {
   Table,
   TableBody,
@@ -10,6 +11,7 @@ import {
   TableHeader,
   TableRow,
 } from 'ui/table';
+import { TypographyH3 } from 'ui/typography';
 
 import { ProfitLossTracker } from 'components/@output/profit-loss-tracker';
 
@@ -31,67 +33,85 @@ const ProfitAndLossPage = async ({
   const totalProfits = data.reduce((sum, row) => sum + row.profit, 0);
 
   return (
-    <div>
-      <Table>
-        <TableCaption>Račun dobiti i gubitka</TableCaption>
-        <TableHeader>
-          <TableRow className="[&>*]:text-right">
-            <TableHead className="w-[100px]">Mjesec</TableHead>
-            <TableHead>Ukupan fiksni trošak</TableHead>
-            <TableHead>Ukupna prodaja</TableHead>
-            <TableHead>Ukupan varijabilni trošak</TableHead>
-            <TableHead>Profit</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {data.map((row) => (
-            <TableRow key={row.month} className="text-right font-mono">
-              <TableCell className="font-medium">{row.month}</TableCell>
-              <TableCell>{formatCurrency(row.total_cost)}</TableCell>
-              <TableCell>{formatCurrency(row.total_sales)}</TableCell>
-              <TableCell>{formatCurrency(row.total_variable_cost)}</TableCell>
-              <TableCell
-                className={row.profit < 0 ? 'text-red-500' : 'text-green-500'}
-              >
-                {formatCurrency(row.profit)}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-          <TableRow className="text-right font-mono">
-            <TableCell colSpan={1}>Total</TableCell>
-            <TableCell>{formatCurrency(totalCosts)}</TableCell>
-            <TableCell>{formatCurrency(totalSales)}</TableCell>
-            <TableCell>{formatCurrency(totalVariableCosts)}</TableCell>
-            <TableCell
-              className={totalProfits < 0 ? 'text-red-500' : 'text-green-500'}
-            >
-              {formatCurrency(totalProfits)}
-            </TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
-      <div className="max-w-sm">
-        <ProfitLossTracker
-          data={data.map((row) => ({
-            color: row.profit < 0 ? 'bg-red-500' : 'bg-green-500',
-            tooltip: (
-              <div>
-                <div>Mjesec: {row.month}</div>
+    <div className="flex flex-col gap-4">
+      <Card>
+        <CardHeader>
+          <TypographyH3>Račun dobiti i gubitka</TypographyH3>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableCaption>Račun dobiti i gubitka</TableCaption>
+            <TableHeader>
+              <TableRow className="[&>*]:text-right">
+                <TableHead className="w-[100px]">Mjesec</TableHead>
+                <TableHead>Ukupan fiksni trošak</TableHead>
+                <TableHead>Ukupna prodaja</TableHead>
+                <TableHead>Ukupan varijabilni trošak</TableHead>
+                <TableHead>Profit</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {data.map((row) => (
+                <TableRow key={row.month} className="text-right font-mono">
+                  <TableCell className="font-medium">{row.month}</TableCell>
+                  <TableCell>{formatCurrency(row.total_cost)}</TableCell>
+                  <TableCell>{formatCurrency(row.total_sales)}</TableCell>
+                  <TableCell>
+                    {formatCurrency(row.total_variable_cost)}
+                  </TableCell>
+                  <TableCell
+                    className={
+                      row.profit < 0 ? 'text-red-500' : 'text-green-500'
+                    }
+                  >
+                    {formatCurrency(row.profit)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow className="text-right font-mono">
+                <TableCell colSpan={1}>Total</TableCell>
+                <TableCell>{formatCurrency(totalCosts)}</TableCell>
+                <TableCell>{formatCurrency(totalSales)}</TableCell>
+                <TableCell>{formatCurrency(totalVariableCosts)}</TableCell>
+                <TableCell
+                  className={
+                    totalProfits < 0 ? 'text-red-500' : 'text-green-500'
+                  }
+                >
+                  {formatCurrency(totalProfits)}
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </CardContent>
+      </Card>
+      <Card className="max-w-sm">
+        <CardHeader>
+          <TypographyH3>Profit i gubitak</TypographyH3>
+        </CardHeader>
+        <CardContent>
+          <ProfitLossTracker
+            data={data.map((row) => ({
+              color: row.profit < 0 ? 'bg-red-500' : 'bg-green-500',
+              tooltip: (
                 <div>
-                  Ukupan fiksni trošak: {formatCurrency(row.total_cost)}
+                  <div>Mjesec: {row.month}</div>
+                  <div>
+                    Ukupan fiksni trošak: {formatCurrency(row.total_cost)}
+                  </div>
+                  <div>
+                    Ukupan varijabilni trošak:{' '}
+                    {formatCurrency(row.total_variable_cost)}
+                  </div>
+                  <div>Ukupna prodaja: {formatCurrency(row.total_sales)}</div>
                 </div>
-                <div>
-                  Ukupan varijabilni trošak:{' '}
-                  {formatCurrency(row.total_variable_cost)}
-                </div>
-                <div>Ukupna prodaja: {formatCurrency(row.total_sales)}</div>
-              </div>
-            ),
-          }))}
-        />
-      </div>
+              ),
+            }))}
+          />
+        </CardContent>
+      </Card>
     </div>
   );
 };
