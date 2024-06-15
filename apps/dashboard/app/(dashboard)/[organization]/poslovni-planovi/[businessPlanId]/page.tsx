c3d400a9bbcebb015a0bac24f93e9d32a17326b2
Mislav <mislavjc@gmail.com>
2024-06-15 10:06:03 +0200

    feat: add breadcrumbs to the business plan

--- apps/dashboard/app/(dashboard)/[organization]/poslovni-planovi/[businessPlanId]/page.tsx
+++ apps/dashboard/app/(dashboard)/[organization]/poslovni-planovi/[businessPlanId]/page.tsx
@@ -3,18 +3,25 @@ import { notFound } from 'next/navigation';
 
 import { getBusinessPlan } from 'actions/plans';
 
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from 'ui/breadcrumb';
 import { Button } from 'ui/button';
-
-import { EditorBlock } from 'components/@plans/content';
-import { Rating } from 'components/@plans/rating';
 import {
   Drawer,
   DrawerContent,
-  DrawerDescription,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from 'components/ui/drawer';
+} from 'ui/drawer';
+
+import { EditorBlock } from 'components/@plans/content';
+import { Rating } from 'components/@plans/rating';
 import { ScrollArea } from 'components/ui/scroll-area';
 
 export const runtime = 'nodejs';
@@ -37,27 +44,50 @@ const BusinessPlanPage = async ({
   }
 
   return (
-    <div className="flex flex-col">
-      <Drawer direction="right">
-        <DrawerTrigger asChild>
-          <Button variant="outline" className="flex gap-2 self-end">
-            <Sparkles />
-            <span>Ocjeni plan</span>
-          </Button>
-        </DrawerTrigger>
-        <DrawerContent className="left-auto right-0 top-0 mt-0 h-screen w-full max-w-lg rounded-none">
-          <ScrollArea className="h-screen">
-            <div className="mx-auto w-full">
-              <DrawerHeader>
-                <DrawerTitle>Ocjenjivanje poslovnog plana</DrawerTitle>
-              </DrawerHeader>
-              <div className="p-4">
-                <Rating businessPlan={businessPlan} />
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-between gap-4">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <div />
+            <BreadcrumbItem>
+              <BreadcrumbLink href={`/${organization}`}>Početna</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href={`/${organization}/poslovni-planovi`}>
+                Poslovni planovi
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>
+                {businessPlan.name ?? 'Bez imena'}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+
+        <Drawer direction="right">
+          <DrawerTrigger asChild>
+            <Button variant="outline" className="flex gap-2 self-end">
+              <Sparkles />
+              <span>Ocjeni plan</span>
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent className="left-auto right-0 top-0 mt-0 h-screen w-full max-w-lg rounded-none">
+            <ScrollArea className="h-screen">
+              <div className="mx-auto w-full">
+                <DrawerHeader>
+                  <DrawerTitle>Ocjenjivanje poslovnog plana</DrawerTitle>
+                </DrawerHeader>
+                <div className="p-4">
+                  <Rating businessPlan={businessPlan} />
+                </div>
               </div>
-            </div>
-          </ScrollArea>
-        </DrawerContent>
-      </Drawer>
+            </ScrollArea>
+          </DrawerContent>
+        </Drawer>
+      </div>
       <div className="mx-auto flex min-h-screen w-full max-w-screen-lg flex-col gap-8 border p-8 md:p-16">
         <EditorBlock businessPlan={businessPlan} organization={organization} />
       </div>
